3f911dd436ae5ad0b5e7afe0e811ff347fd2e3ef
abeyaworld <118046008+abeyaworld@users.noreply.github.com>
2022-12-30 14:19:52 +0900

    JavaScript学習

---
+++ app/services/search_tweets_service.rb
@@ -0,0 +1,12 @@
+class SearchTweetsService
+  def self.search(search)
+    if search != ""
+      search = "%#{search}%"
+      Tweet.find_by_sql(["select * from tweets where text like ? ", search])
+      # 危険 Tweet.find_by_sql("select * from tweets where text like '%#{search}%' ")
+      # Tweet.where('text LIKE(?)', "%#{search}%")
+    else
+      Tweet.all
+    end
+  end
+end
